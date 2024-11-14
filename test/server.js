4c00572e35a831905148f75594866294c8f13f3b
saverious <saverious7@gmail.com>
2024-11-14 16:50:48 +0300

    ref: modify server setup

--- test/server.js
+++ test/server.js
@@ -1,17 +1,7 @@
 import express from 'express';
-import limiter from '../index.js';
 
 // set global variables
 const app = express();
-
-// set custom rate limiting values
-app.use(limiter({
-    windowMS: 20,
-    limit: 5
-}));
-
-// use default settings for limiter
-// app.use(limiter());
 
 // endpoint
 app.get('/', function(req, res) {
--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import axios from 'axios';
 import app from './server.js';
+import limiter from '../index.js';
 
 // set global variables
 let port, url;
@@ -9,17 +10,28 @@ let port, url;
 beforeEach(() => {
     port = 3000;
     url = `http://localhost:${port}`;
-
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`)
-    });
 });
 
 // tests
-describe('make request', () => {
-    it('make http call to endpoint', async () => {
-        const res = await axios.get(url);
-        expect(res.status).to.equal(200);
+describe('*** make http requests ***', () => {
+    it('make consecutive http calls to endpoint', async () => {
+        // set rate limiter middleware
+        app.use(limiter({
+            windowMS: 20,
+            limit: 5
+        }));
+
+        // start server
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        });
+
+        // make 10 consecutive requests
+        for(let i = 0; i < 10; i++){
+            let res = await axios.get(url);
+            console.log('status code: ', res.status);
+            expect(res.status).to.equal(200);
+        }
     });
 });
 
